d8a0c665997997a7722cef3b8d0de441dce16e0b
PRIZEOX <oganisyanararat1@gmail.com>
2022-12-23 11:46:03 +0400

    forma

---
+++ FAQU/Auth.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace FAQU
+{
+    partial class Auth
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.enterButton = new System.Windows.Forms.Button();
+            this.passField = new System.Windows.Forms.TextBox();
+            this.loginField = new System.Windows.Forms.TextBox();
+            this.loginLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // enterButton
+            // 
+            this.enterButton.Location = new System.Drawing.Point(339, 296);
+            this.enterButton.Name = "enterButton";
+            this.enterButton.Size = new System.Drawing.Size(140, 33);
+            this.enterButton.TabIndex = 0;
+            this.enterButton.Text = "Войти";
+            this.enterButton.UseVisualStyleBackColor = true;
+            this.enterButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // passField
+            // 
+            this.passField.Location = new System.Drawing.Point(323, 250);
+            this.passField.Name = "passField";
+            this.passField.Size = new System.Drawing.Size(173, 20);
+            this.passField.TabIndex = 1;
+            // 
+            // loginField
+            // 
+            this.loginField.Location = new System.Drawing.Point(323, 190);
+            this.loginField.Name = "loginField";
+            this.loginField.Size = new System.Drawing.Size(173, 20);
+            this.loginField.TabIndex = 2;
+            // 
+            // loginLabel
+            // 
+            this.loginLabel.AutoSize = true;
+            this.loginLabel.Location = new System.Drawing.Point(386, 162);
+            this.loginLabel.Name = "loginLabel";
+            this.loginLabel.Size = new System.Drawing.Size(38, 13);
+            this.loginLabel.TabIndex = 3;
+            this.loginLabel.Text = "Логин";
+            this.loginLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Location = new System.Drawing.Point(386, 225);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(45, 13);
+            this.passwordLabel.TabIndex = 4;
+            this.passwordLabel.Text = "Пароль";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.loginLabel);
+            this.Controls.Add(this.loginField);
+            this.Controls.Add(this.passField);
+            this.Controls.Add(this.enterButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Auth_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button enterButton;
+        private System.Windows.Forms.TextBox passField;
+        private System.Windows.Forms.TextBox loginField;
+        private System.Windows.Forms.Label loginLabel;
+        private System.Windows.Forms.Label passwordLabel;
+    }
+}
+
